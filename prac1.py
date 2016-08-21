bae9d35ed75ea3dce92dce70b93c229699fc0b05
jc320816 <tristin.hillebrand@my.jcu.edu.au>
2016-08-22 09:27:25 +1000

    number update

--- prac1.py
+++ prac1.py
@@ -1,3 +1,8 @@
-"""word thing
 """
+word thing
+https://github.com/TristinHillebrand/prac5
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers[0], numbers[-1], numbers[3], numbers[:-1], numbers[3:4], 5 in numbers, 7 in numbers, "3" in numbers, numbers + [6, 5, 3], numbers[-7])
 
